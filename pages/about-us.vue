aba291a14c625adcc8e2f0c3889537321a4c24ec
Maksym Usyk <max.taina32@gmail.com>
2025-04-02 21:46:09 +0300

    about us changes

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -2,11 +2,12 @@
 	<div class="flex justify-center items-center h-screen">
 		<Dialog>
 			<div class="w-full h-full flex flex-col justify-center items-center">
-				<p>Welcome to our weather forecast website!</p>
-				<p>We created this project to make checking the weather simple, accurate, and useful for everyday planning. With a clean and intuitive interface, our platform provides real-time weather updates, a 5-day forecast, and personalized clothing recommendations tailored to the conditions in your city.</p>
-				<p>Built with Nuxt.js, Tailwind CSS, and external APIs, the website is designed for a seamless experience across all devices. From UI design to deployment on Vercel, every detail was crafted to ensure fast performance and ease of use.</p>
-				<p>Whether you're planning your day or packing for a trip, our goal is to help you stay prepared with the right information at the right time.</p>
-				<p>Thank you for visiting — stay weather-ready!</p>
+				<p>Hey there! 👋</p>
+				<p>I built this weather forecast website to make checking the weather simple, accurate, and useful for your daily plans. With a clean and intuitive interface, you can get real-time weather updates, a 5-day forecast, and even personalized clothing recommendations based on the conditions in your city. 🧥🌡️</p>
+				<p>The site is powered by Nuxt.js, Tailwind CSS, and external APIs to ensure a smooth experience on any device. It uses OpenWeather for weather data and Mapbox for location services, so you always get the most precise and up-to-date forecasts. 🗺️☀️ To keep things secure and efficient, I’ve also implemented a proxy server that adds an extra layer of protection for user requests. 🔒</p>
+				<p>From designing the UI to deploying the site on Vercel, I’ve focused on speed, usability, and reliability. Whether you're planning your day or packing for a trip, I’ve got you covered with the right info at the right time. ✅</p>
+				<p>I also use images & icons from <u><a href="https://www.freepik.com/">Freepik</a></u> to enhance the visual experience, making the site both functional and aesthetically pleasing. 🎨✨</p>
+				<p>Thanks for stopping by—stay weather-ready! 🌦️🚀</p>
 			</div>
 		</Dialog>
 	</div>
